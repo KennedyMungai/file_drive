bc5dd0bd7bccef5abda190dc6200855f67044ba5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 16:17:33 +0300

    Setup shadcn for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-			<html lang='en'>
-				<body className={inter.className}>
-					<ConvexClientProvider>{children}</ConvexClientProvider>
-				</body>
-			</html>
+		<html lang='en'>
+			<body className={inter.className}>
+				<ConvexClientProvider>{children}</ConvexClientProvider>
+			</body>
+		</html>
 	)
 }
