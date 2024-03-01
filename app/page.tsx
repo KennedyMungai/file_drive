ec6fcf87e5bdb6626baa92990711949cff932fe7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-01 16:23:14 +0300

    Added a sign in button to the index page of the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { SignInButton } from '@clerk/nextjs'
+
 export default function Home() {
-	return <main className='min-h-screen'></main>
+	return (
+		<main className='min-h-screen'>
+			<SignInButton mode='modal' />
+		</main>
+	)
 }
