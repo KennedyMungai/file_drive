eaac76c4b9f9d59d28cc9b60c2e3b1af8b33634e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 10:04:30 +0300

    Added an import for the Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import Header from './Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
