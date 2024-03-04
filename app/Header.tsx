1b8b5e708653f38418fe89b538ade2dd12ac8a2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 10:06:44 +0300

    Added some styles for the Header component

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const Header = (props: Props) => {
-	return <div>Header</div>
+	return (
+		<div className='border-b'>
+			<div className='container mx-auto'>Hello World</div>
+		</div>
+	)
 }
 
 export default Header
