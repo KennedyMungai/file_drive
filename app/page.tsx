d453cb528bca63359be6bd53408ca64057e2650e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 09:00:21 +0300

    Added a bunnch of sign in and sign out buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import { SignInButton } from '@clerk/nextjs'
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+	SignInButton,
+	SignOutButton,
+	SignedIn,
+	SignedOut,
+	useAuth,
+	useUser
+} from '@clerk/nextjs'
 
 export default function Home() {
+	const { userId, isSignedIn } = useAuth()
+	const { user } = useUser()
+
 	return (
 		<main className='min-h-screen'>
-			<SignInButton mode='modal' />
+			<SignedIn>
+				<SignOutButton />
+			</SignedIn>
+			<SignedOut>
+				<SignInButton mode='modal' />
+			</SignedOut>
+			<Button>Hello World</Button>
 		</main>
 	)
 }
